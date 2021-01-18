cf455addebd7f9bef1d1a9371c8a95d5b8892e7b
Miruna Vlc <vlaicu_miruna@yahoo.com>
2021-01-18 19:21:22 +0200

    Add tests ExtractProperty_USDEURPropertyShouldBeSeparated ExtractProperty_EURPropertyShouldBeSeparated

---
+++ ExchangeRate.Tests/LoaderTests.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace ExchangeRate.Tests
+{
+    public class LoaderTests
+    {
+        [Fact]
+        public void ExtractProperty_USDEURPropertyShouldBeSeparated()
+        {
+            //Arrange
+            double expected = 0.847802;
+
+            //Act
+            string json =
+            @"{
+                ""success"": true,
+                ""terms"": ""https:\/\/currencylayer.com\/terms"",
+                ""privacy"": ""https:\/\/currencylayer.com\/privacy"",
+                ""timestamp"": 1507639147,
+                ""source"": ""USD"",
+                ""quotes"":
+                    {
+                    ""USDETB"": 23.410168,
+                    ""USDEUR"": 0.847802,
+                    ""USDFJD"": 2.052013,
+                    ""USDFKP"": 0.757298
+                    }
+              }";
+
+            double actual = Loader.ExtractProperty<double>(json, "USDEUR");
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ExtractProperty_EURPropertyShouldBeSeparated()
+        {
+            //Arrange
+            var expected = 0.848383;
+
+            //Act
+            string json = 
+            @"{
+                ""disclaimer"": ""Usage subject to terms: https://openexchangerates.org/terms"",
+                ""license"": ""https://openexchangerates.org/license"",
+                ""timestamp"": 1507640400,
+                ""base"": ""USD"",
+                ""rates"": 
+                {
+                    ""ETB"": 23.66,
+                    ""EUR"": 0.848383,
+                    ""FJD"": 2.056501,
+                    ""FKP"": 0.758728,
+                    ""GBP"": 0.758728,
+                    ""GEL"": 2.475408,
+                    ""GGP"": 0.758728
+                }
+               }";
+
+            double actual = Loader.ExtractProperty<double>(json, "EUR");
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ ExchangeRate/Loader.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ExchangeRate
+{
+    public class Loader
+    {
+        public static T ExtractProperty<T>(string json, string propertyName) 
+        {
+            var root = JToken.Parse(json);
+            var tokens = root.SelectTokens(".." + propertyName);
+            var token = tokens.Any() ? tokens.First() : null;
+            T actual = default(T);
+            if (token != null)
+            {
+                actual = token.ToObject<T>();
+            }
+            return actual;
+        }
+    }
+}
