84e3552eebb62dd68d26a79f9099245d24f136a6
Miruna Vlc <vlaicu_miruna@yahoo.com>
2021-01-18 15:34:47 +0200

    add .net console app with basic output

---
+++ ExchangeRate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ExchangeRate
+{
+    class Program
+    {
+        internal enum PrintModes
+        {
+            [Description("Console")]
+            Console,
+
+            [Description("MessageBox")]
+            MessageBox
+        }
+
+        public static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+            if (attributes != null && attributes.Any())
+            {
+                return attributes.First().Description;
+            }
+
+            return value.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            var printMode = ConfigurationManager.AppSettings["PrintMode"];
+            if(printMode == GetEnumDescription(PrintModes.Console))
+            {
+                Console.WriteLine("Hello");
+            }
+            else
+            {
+                MessageBox.Show("Hello");
+            }
+        }
+    }
+}
