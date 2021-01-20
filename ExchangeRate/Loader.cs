84e02eb156a30c4941a5038e141873b5d33dc0a1
Miruna Vlc <vlaicu_miruna@yahoo.com>
2021-01-20 19:01:13 +0200

    add logging to Loader

--- ExchangeRate/Loader.cs
+++ ExchangeRate/Loader.cs
@@ -1,19 +1,30 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using ExchangeRate.Helpers;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Linq;
 
 namespace ExchangeRate
 {
     public class Loader
     {
+        private static readonly ILogger _logger = new Logger("Loader");
+
         public static T ExtractProperty<T>(string json, string propertyName)
         {
-            var root = JToken.Parse(json);
-            var tokens = root.SelectTokens(".." + propertyName);
-            var token = tokens.Any() ? tokens.First() : null;
             T actual = default(T);
-            if (token != null)
+            try
             {
-                actual = token.ToObject<T>();
+                var root = JToken.Parse(json);
+                var tokens = root.SelectTokens(".." + propertyName);
+                var token = tokens.Any() ? tokens.First() : null;
+                if (token != null)
+                {
+                    actual = token.ToObject<T>();
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.Warn(ex.Message + "Method <ExtractProperty> found no property={0} in json={1}.", propertyName, json);
             }
             return actual;
         }
